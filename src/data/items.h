58a96063d87482699f7956e07980e0421d72df10
Kaixer <EmeraldRomHack@gmail.com>
2025-07-25 19:29:59 +0200

    only commit

--- src/data/items.h
+++ src/data/items.h
@@ -142,7 +142,7 @@ const struct Item gItemsInfo[] =
 {
     [ITEM_NONE] =
     {
-        .name = _("????????"),
+        .name = _("Nothing"),
         .price = 0,
         .description = sQuestionMarksDesc,
         .pocket = POCKET_ITEMS,
